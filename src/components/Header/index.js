839bc14a2c8ba7a1a32ee1c199f86e704d86e260
Julia Magalhães <juliamagalhaes28@outlook.com>
2021-05-11 23:08:36 -0300

    imagem corrigida

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
+import Pipefy from '../../assets/img/pipefy.png';
 import { Container } from './styles'
 
 export default function Header(){
     return(
         <Container>
-            {/* <h1>Pipefy</h1> */}
-            <img src="https://www.filepicker.io/api/file/gdCSpkjSjir1lfGMMLzp" />
+            <img src={Pipefy} alt="Pipefy" />
         </Container>
     )
 }
